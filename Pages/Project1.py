b2581109f3d63e8d16493a4ba464c1647f2051a2
STARLINECOMP <evdimid@mail.ru>
2024-11-01 14:14:57 +0600

    сссс

--- Pages/Project1.py
+++ Pages/Project1.py
@@ -7,15 +7,9 @@ class Project1:
     def __init__(self):
         pass
     def app(self):
-
-
         st.write('Project1')
-        def load_data():
-            # Имитация длительной загрузки данных
-            data = pd.DataFrame({'smth': [11, 28, np.nan, 48, 59, 101, 58, 32]})
-            return data
 
-        df = load_data()
-        st.dataframe(df)
+
+
 
 
