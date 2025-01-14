edb68c8950dbb3fc73700b2c6357c0ad8953023a
STARLINECOMP <evdimid@mail.ru>
2025-01-14 17:55:14 +0600

    14.01.2025 (3)

--- App.py
+++ App.py
@@ -28,6 +28,11 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-N9DG6LQG');</script>
 <!-- End Google Tag Manager -->
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9DG6LQG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 """
 
 # Inject the Google Analytics script into the page
